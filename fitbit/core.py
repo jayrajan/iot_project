d9ef5124ed95722d6a38449986c64a2765e00adc
Jerin Rajan <jrajan@jprtech.co.uk>
2020-05-28 10:25:50 +0100

    feat: convert response from binary to JSON format

--- fitbit/core.py
+++ fitbit/core.py
@@ -1,47 +1,42 @@
-import fitbit
+# Code written by Jerin Rajan 
 import urllib.request
 from urllib.parse import urlencode
-import urllib3
 import requests
 import json
-import ssl
 import oauthlib
 import http.client
 from requests_oauthlib import OAuth2Session
 import base64
 import re
+
 # FITBIT URLs
 fit_redirect_uri = 'https://127.0.0.1:8080/callback'
-fitbit_url = 'https://www.fitbit.com'
-fitbit_api_url = 'api.fitbit.com'
-url1 = 'https://www.fitbit.com/oauth2/authorize?'
+fit_auth_url = 'https://www.fitbit.com/oauth2/authorize?'
 fit_token_url =  'https://api.fitbit.com/oauth2/token'
-# 'https://www.fitbit.com/oauth2/token'
 
 # APPLICATION DETAILS
 # OAuth 2.0 Client ID - Identify the application
 CLIENT_ID = '22BR7W'
 # Client Secret - Secret key which is stored on server side securely & not available to public
 CLIENT_SECRET = 'd608a786153c83f7ed91ddfdcb839e29'
+
+# BASE64 ENCODE - CLIENT ID : CLIENT SECRET
 data_64 = (CLIENT_ID+":"+CLIENT_SECRET)
 encodedBytes = base64.b64encode(data_64.encode("utf-8"))
 base64_code_str = str(encodedBytes,'utf-8')
-print(base64_code_str)
-# base64_code = base64.b64encode(bytes((CLIENT_ID+":"+CLIENT_SECRET),'utf-8'))
 
-responsetype = 'token'
-expirytime_ms = '604800' 
+# SCOPE - HEARTRATE
 fit_scope = ['heartrate']
-# auth_url = url1+'?response_type='+responsetype+'&client_id='+CLIENT_ID+'&redirect_uri='+fit_redirect_uri+'&scope=heartrate'+'&expires_in='+expirytime_ms
 
+# OAUTH2 AUTHENTICATION
 # creating an OAuth2Session object
 fitbit = OAuth2Session(client_id=CLIENT_ID, redirect_uri=fit_redirect_uri, scope=fit_scope)
 # Generating authorisation url
-authorisation_url, state = fitbit.authorization_url(url1)
+authorisation_url, state = fitbit.authorization_url(fit_auth_url)
 print('Please go to %s and authorize access.' %authorisation_url)
+
 # Asking for the Callback URL response
 authorisation_response = input('Enter full callback URL response: ')
-# print(authorisation_response)
 
 # Regular expression - extracting the authorisation code - ONLY
 authorisation_code = re.findall('code=[a-z0-9]+',authorisation_response)
@@ -49,7 +44,6 @@ auth_code= re.findall('[a-z0-9]+',authorisation_code[0])
 
 # Authorisation code
 auth_code_num = auth_code[1]
-print(auth_code_num)
 
 # Data Payloads
 data = {'code' : auth_code_num,
@@ -58,7 +52,6 @@ data = {'code' : auth_code_num,
         'state' : state,
         'grant_type' : 'authorization_code'}
 dataURLencoded = urlencode(data).encode('utf-8')
-# print(dataURLencoded)
 
 # Create a request object
 req = urllib.request.Request(fit_token_url,data=dataURLencoded, method='POST')
@@ -66,12 +59,15 @@ req = urllib.request.Request(fit_token_url,data=dataURLencoded, method='POST')
 # Authorisation Header
 req.add_header('Authorization', 'Basic '+base64_code_str)
 req.add_header('Content-Type', 'application/x-www-form-urlencoded')
-print(req.get_full_url())
-# POST Request
+
+# POST TOKEN Request
 try:
     response = urllib.request.urlopen(req)
     FullResponse = response.read()
-    print(FullResponse)
+    # Printing response in JSON format
+    js_response = json.loads(FullResponse)
+    print('Response:',json.dumps(js_response,indent=4))
+
 except urllib.error.HTTPError as e:
     print('HTTP ERROR CODE:',e.code)
     print(e.msg)
